96cdfda0ab8478ccb66bb305db04081b05ffbd65
Teokim2003 <tgkim@uncg.edu>
2025-06-02 20:55:09 -0400

    Create DogRepository.java

---
+++ src/main/java/com/example/demo/repository/DogRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Dog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DogRepository extends JpaRepository<Dog, Long> {
+    List<Dog> findByCategory(String category);
+    List<Dog> findByNameContainingIgnoreCase(String name);
+}

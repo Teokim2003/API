4b4fcc834fad04b2daae4007a09ff7d0edacb5d2
Teokim2003 <tgkim@uncg.edu>
2025-06-09 20:28:59 -0400

    Changing from RestController to Controller

--- src/main/java/com/example/demo/controller/DogController.java
+++ src/main/java/com/example/demo/controller/DogController.java
@@ -1,13 +1,22 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.Dog;
-import com.example.demo.service.DogService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
-@RestController
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.Dog;
+import com.example.demo.service.DogService;
+
+@Controller
 @RequestMapping("/api/dogs")
 public class DogController {
 
